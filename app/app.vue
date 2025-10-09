2190f26d42aa7f9296deb4fc13d59c213ba4dcae
rauanto <74162376+raulanto@users.noreply.github.com>
2025-10-09 12:46:30 -0600

    analiticas
    Analytics

--- app/app.vue
+++ app/app.vue
@@ -3,4 +3,11 @@
         <UBanner title="Proyecto en Construccion" />
         <NuxtPage />
     </UApp>
+    <Analytics/>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from '@vercel/analytics/nuxt'
+
+
+</script>
